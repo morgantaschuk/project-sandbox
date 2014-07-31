5181177b9bc0d764223250afc176570b71dc4aba
Morgan Taschuk <mtaschuk@oicr.on.ca>
2014-07-31 19:33:17 -0400

    Added error checking for bad zip files; fixed the usage information

--- annotation-project/annotate_reads.py
+++ annotation-project/annotate_reads.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-import sys,getopt
+import sys,getopt,time
 from subprocess import call
 import csv,zipfile,re
 
-def usage():
-   print 'annotate_reads.py -i <inputfile> -o <outputfile>'
-
+def usage(long_opts):
+   print 'annotate_reads.py',"[","--"+" --".join(long_opts).replace("="," <val>"),"]"
+   print '   use -h to print this message'
 
 def open_fastqc(fastqc_zip,match_strings):
    """ 
@@ -15,38 +15,43 @@ def open_fastqc(fastqc_zip,match_strings):
    """
    annotations={}
    #open the fastqc zip
-   with zipfile.ZipFile(fastqc_zip,'r') as myzip:
-      #since the fastqc_data.txt file is in a directory with a custom name
-      #find the path in the zip of the file so we can open it
-      matching = filter(lambda element: "fastqc_data.txt" in element, myzip.namelist())
-      #open the fastqc_data.txt file and find the relevant data
-      for line in myzip.read(matching[0],'rU').split("\n"):
-	 #stop parsing if all of the matches have been made
-         if len(annotations.keys()) == len(match_strings):
-            break
-         else:
-            #test each line against the list of potential matches
-	    #add it to the dict if it matches
-            for match_str in match_strings:
-               match = re.match(match_str,line)
-               if match:
-                  annotations[match.group(1)]=match.group(2).rstrip()
+   try:
+      with zipfile.ZipFile(fastqc_zip,'r') as myzip:
+         #since the fastqc_data.txt file is in a directory with a custom name
+         #find the path in the zip of the file so we can open it
+         matching = filter(lambda element: "fastqc_data.txt" in element, myzip.namelist())
+         #open the fastqc_data.txt file and find the relevant data
+         for line in myzip.read(matching[0],'rU').split("\n"):
+   	    #stop parsing if all of the matches have been made
+            if len(annotations.keys()) == len(match_strings):
+               break
+            else:
+               #test each line against the list of potential matches
+	       #add it to the dict if it matches
+               for match_str in match_strings:
+                  match = re.match(match_str,line)
+                  if match:
+                     annotations[match.group(1)]=match.group(2).rstrip()
+   except zipfile.BadZipfile as e:
+      print " : ".join([time.strftime("%x %X"), "ERROR: BadZipfile", str(e), fastqc_zip])
    return annotations
 
 def main(argv):
-   inputfile = None
-   outputfile = None
    reportarg = []
+   test=False
+   long_opts=["test","study-name=","root-sample-name=","sample-name=","sequencer-run-name=","ius-SWID=","lane-SWID="]
    try:
-      opts, args = getopt.getopt(argv,"h",["ius-SWID="])
+      opts, args = getopt.getopt(argv,"h",long_opts)
    except getopt.GetoptError:
-      usage()
+      usage(long_opts)
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         usage()
+         usage(long_opts)
          sys.exit()
-      elif opt in ("--ius-SWID"):
+      elif opt == '--test':
+         test=True
+      else:
          reportarg.append(opt)
 	 reportarg.append(arg)
    #generate the report
@@ -71,12 +76,15 @@ def main(argv):
 	 #annotate the file with information pulled out of the FastQC report
          if annos is not None:
             for key in annos.keys():
-               s = ",".join([line['File SWID'], key.replace(" ", "_"), annos[key].replace(" ", "_"),"\n"])
+               s = ",".join([line['File SWID'], key.replace(" ", "_"), annos[key].replace(" ", "_").replace("_/_","/"),"\n"])
                anno_file.write(s);
             
    anno_file.close()
    mycall = ["seqware", "annotate", "file", "--csv", "annotations.csv"] 
-   call(mycall)
+   if not test:
+      call(mycall)
+   else:
+      print "Test mode enabled:",str(mycall)
 
 
 if __name__ == "__main__":
